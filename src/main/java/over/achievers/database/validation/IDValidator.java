ba2044d46b0a2f84e60389c77fba9ae14691f4f7
Vlad Logyin <vladlogyin@gmail.com>
2022-07-11 14:10:32 +0100

    Implement employee ID validator

--- src/main/java/over/achievers/database/validation/IDValidator.java
+++ src/main/java/over/achievers/database/validation/IDValidator.java
@@ -1,4 +1,22 @@
 package over.achievers.database.validation;
 
+import over.achievers.database.model.Employee;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IDValidator implements Validator{
+    Map<Integer,Boolean> previousIDs;
+    public IDValidator()
+    {
+        previousIDs = new HashMap<>();
+    }
+    @Override
+    public boolean isValid(Employee employee) {
+        if(previousIDs.containsKey(employee.getEmpNumber()))
+            return false;
+        previousIDs.put(employee.getEmpNumber(),true);
+        return true;
+
+    }
 }
