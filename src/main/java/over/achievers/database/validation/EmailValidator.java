f11a1fc3ffaad398ac30c66282ef6bc036c41fc6
Baaartosz <bartosz_p@hotmail.com>
2022-07-11 13:58:57 +0100

    added emailValidator

---
+++ src/main/java/over/achievers/database/validation/EmailValidator.java
@@ -0,0 +1,11 @@
+package over.achievers.database.validation;
+
+import over.achievers.database.DatabaseEntry;
+
+public class EmailValidator implements Validator{
+
+    @Override
+    public boolean isValid(DatabaseEntry entry) {
+
+    }
+}
