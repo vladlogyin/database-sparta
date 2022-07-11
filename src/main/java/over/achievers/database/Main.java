4881a958ad7f8702ea54684e333cd7eaf0d2b6ca
Vlad Logyin <vladlogyin@gmail.com>
2022-07-11 17:13:32 +0100

    Sprinkle collections everywhere

--- src/main/java/over/achievers/database/Main.java
+++ src/main/java/over/achievers/database/Main.java
@@ -3,11 +3,15 @@ package over.achievers.database;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         String filenameCSV="asd.csv";
         InputStream inputCSV = new FileInputStream(filenameCSV);
-        DatabaseImporter dbImporter = new DatabaseImporter(inputCSV);
+        Scanner sc = new Scanner(inputCSV);
+        while(sc.hasNextLine())
+            
+        //DatabaseImporter dbImporter = new DatabaseImporter(inputCSV);
     }
 }
