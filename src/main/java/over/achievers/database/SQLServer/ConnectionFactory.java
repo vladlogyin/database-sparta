06148e7202a4f0b7522f3252a86f7c2808f6f88d
mustafa <travellingtypist1@gmail.com>
2022-07-12 15:44:21 +0100

    new commit

---
+++ src/main/java/over/achievers/database/SQLServer/ConnectionFactory.java
@@ -0,0 +1,40 @@
+package com.sparta.sakila;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+
+public class ConnectionFactory implements AutoCloseable {
+    public static Connection connection = null;
+    public static Connection getConnection(){
+        Properties dbProps = new Properties();
+        try {
+            dbProps.load(new FileReader("src/main/java/com/sparta/sakila/database.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (connection == null){
+            try{
+                connection = DriverManager.getConnection(dbProps.getProperty("db.url"), dbProps.getProperty("db.username"), dbProps.getProperty("db.password"));
+            } catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public void close(){
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
