cbf308102d5877331a9594deebfcaff2308a63f6
Baaartosz <bartosz_p@hotmail.com>
2022-07-13 16:03:28 +0100

    implemented MainView methods getFilePathFromUser() displayStatistics() informProcessingStart() informProcessingEnd()

--- src/main/java/over/achievers/database/viewer/MainViewer.java
+++ src/main/java/over/achievers/database/viewer/MainViewer.java
@@ -6,11 +6,31 @@ public class MainViewer {
 
     Scanner scanner = new Scanner(System.in);
 
+    public String getFilePathFromUser(){
+        System.out.print("Please enter the path to your employee.csv\n> ");
+        return scanner.nextLine();
+    }
 
-    public String getUserChoice(){
-        System.out.println("What would you like?\n1. Move data from file\n2. Input a new employee record\n3. Get an employee record");
+    public void informProcessingStart(){
+        System.out.println("Processing employee data.");
+    }
+
+    public void informProcessingComplete(){
+        System.out.println("Finished processing employee data.");
+    }
+
+    private String getUserChoice(){
+        System.out.println("What action would you like to preform?\n1. Move data from file\n2. Input a new employee record\n3. Get an employee record");
         String choice = scanner.nextLine();
         return choice;
+    }
+
+    public void displayStatistics(int unique, int clean, int duplicates, int missingFields){
+        System.out.println("Migration Statistics");
+        System.out.format("\n%-34s: %-10d\n", "Unique records" ,unique);
+        System.out.format("%-34s: %-10d\n", "Clean records" ,clean);
+        System.out.format("%-34s: %-10d\n", "Duplicate records" ,duplicates);
+        System.out.format("%-34s: %-10d\n", "Records with missing fields",missingFields);
     }
 
 }
