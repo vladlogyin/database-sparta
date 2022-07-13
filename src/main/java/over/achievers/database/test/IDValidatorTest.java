6016e9a1c5446b04583da83155732234c0511fd6
OTDZ <omartehami6@gmail.com>
2022-07-13 16:49:53 +0100

    Tested validity of an employee with a duplicate ID

--- src/main/java/over/achievers/database/test/IDValidatorTest.java
+++ src/main/java/over/achievers/database/test/IDValidatorTest.java
@@ -25,7 +25,7 @@ class IDValidatorTest {
     @DisplayName("Positive integer ID")
     void testPositiveIntegerID(){
         // ID: 199
-        Employee employee = employeeParser.parse("199,Mr.,Khabib,A,Nurmagomedov,M,rjackson77@hotmail.com,10/10/1982,4/1/2009,100123");
+        Employee employee = employeeParser.parse("199,Mr.,Khabib,A,Nurmagomedov,M,knurm77@hotmail.com,10/10/1982,4/1/2009,100123");
         Assertions.assertEquals(true, idValidator.isValid(employee));
     }
 
@@ -33,7 +33,7 @@ class IDValidatorTest {
     @DisplayName("Negative integer ID")
     void testNegativeIntegerID(){
         // ID: -500
-        Employee employee = employeeParser.parse("-500,Mr.,Khabib,A,Nurmagomedov,M,rjackson77@hotmail.com,10/10/1982,4/1/2009,100123");
+        Employee employee = employeeParser.parse("-500,Mr.,Khabib,A,Nurmagomedov,M,knurm77@hotmail.com,10/10/1982,4/1/2009,100123");
         Assertions.assertEquals(false, idValidator.isValid(employee));
     }
 
@@ -41,8 +41,18 @@ class IDValidatorTest {
     @DisplayName("0 ID")
     void testZeroID(){
         // ID: 0
-        Employee employee = employeeParser.parse("0,Mr.,Khabib,A,Nurmagomedov,M,rjackson77@hotmail.com,10/10/1982,4/1/2009,100123");
+        Employee employee = employeeParser.parse("0,Mr.,Khabib,A,Nurmagomedov,M,knurm77@hotmail.com,10/10/1982,4/1/2009,100123");
         Assertions.assertEquals(true, idValidator.isValid(employee));
+    }
+
+    @Test
+    @DisplayName("Duplicate ID")
+    void testDuplicateID(){
+        // ID: 1000
+        Employee employee1 = employeeParser.parse("1000,Mr.,Khabib,A,Nurmagomedov,M,knurm77@hotmail.com,10/10/1982,4/1/2009,100123");
+        Employee employee2 = employeeParser.parse("1000,Mr.,Conor,D,McGregor,M,c_mc_gregor@hotmail.com,1/12/1980,8/4/2004,90019");
+        Assertions.assertEquals(true, idValidator.isValid(employee1));
+        Assertions.assertEquals(false, idValidator.isValid(employee2));
     }
 
 }
