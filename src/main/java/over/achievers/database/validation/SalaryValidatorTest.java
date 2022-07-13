e5d7626cf4c898ab7ea3b3da48fc6e2e34957544
OTDZ <omartehami6@gmail.com>
2022-07-13 16:55:14 +0100

    Tested validity of an employee with a salary as a positive integer

---
+++ src/main/java/over/achievers/database/validation/SalaryValidatorTest.java
@@ -0,0 +1,30 @@
+package over.achievers.database.validation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import over.achievers.database.model.Employee;
+import over.achievers.database.parsing.Parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SalaryValidatorTest {
+
+    SalaryValidator salaryValidator;
+    Parser employeeParser;
+
+    @BeforeEach
+    void setUp() {
+        salaryValidator = new SalaryValidator();
+        employeeParser = new Parser();
+    }
+
+    @Test
+    @DisplayName("Positive integer salary")
+    void testPositiveIntegerSalary(){
+        Employee employee = employeeParser.parse("1,Mrs.,Ronda,W,Jackson,F,rjackson77@hotmail.com,10/10/1982,4/1/2009,100123");
+        Assertions.assertEquals(true, salaryValidator.isValid(employee));
+    }
+
+}
