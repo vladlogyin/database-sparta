f944b2a41a323619931fcca29aeed6ef4e1dd4d9
OTDZ <omartehami6@gmail.com>
2022-07-13 16:40:11 +0100

    Tested validity of employee with a positive integer as ID

---
+++ src/main/java/over/achievers/database/test/IDValidatorTest.java
@@ -0,0 +1,32 @@
+package over.achievers.database.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import over.achievers.database.model.Employee;
+import over.achievers.database.parsing.Parser;
+import over.achievers.database.validation.IDValidator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IDValidatorTest {
+
+    IDValidator idValidator;
+    Parser employeeParser;
+
+    @BeforeEach
+    void setUp() {
+        idValidator = new IDValidator();
+        employeeParser = new Parser();
+    }
+
+    @Test
+    @DisplayName("Positive integer ID")
+    void testPositiveIntegerID(){
+        // ID: 199
+        Employee employee = employeeParser.parse("199,Mr.,Khabib,A,Nurmagomedov,M,rjackson77@hotmail.com,10/10/1982,4/1/2009,100123");
+        Assertions.assertEquals(true, idValidator.isValid(employee));
+    }
+
+}
