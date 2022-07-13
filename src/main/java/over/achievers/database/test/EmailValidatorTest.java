a0dc53a1ef7f4a7bb68e3d435fb9261277298652
OTDZ <omartehami6@gmail.com>
2022-07-13 15:47:41 +0100

    Tested validity of employee with an only symbolic email

--- src/main/java/over/achievers/database/test/EmailValidatorTest.java
+++ src/main/java/over/achievers/database/test/EmailValidatorTest.java
@@ -39,4 +39,12 @@ class EmailValidatorTest {
         Assertions.assertEquals(false, emailValidator.isValid(employee));
     }
 
+    @Test
+    @DisplayName("Symbolic email")
+    void testSymbolicEmail(){
+        // Email: 12345
+        Employee employee = employeeParser.parse("2,Mr.,Russel,D,Westbrook,M,~@@~:}{:@,12/21/976,9/11/2012,909010");
+        Assertions.assertEquals(false, emailValidator.isValid(employee));
+    }
+
 }
