f7e5df2cd849ee6baff863dd91b0055b524274fb
OTDZ <omartehami6@gmail.com>
2022-07-13 15:16:52 +0100

    Tested validity of employee with joining date in the future

--- src/main/java/over/achievers/database/validation/DateValidatorTest.java
+++ src/main/java/over/achievers/database/validation/DateValidatorTest.java
@@ -43,8 +43,16 @@ class DateValidatorTest {
     @DisplayName("Join date before future")
     void testJoiningDateBeforeFuture(){
         // Joining Date: 1/25/1988
-        Employee employee = employeeParser.parse("3,Mr.,Nikola,L,Jokic,M,nikjok6892@gmail.com,7/28/1960,1/25/1988,20918");
+        Employee employee = employeeParser.parse("3,Mr.,Nikola,O,Jokic,M,nikjok6892@gmail.com,7/28/1960,1/25/1988,20918");
         Assertions.assertEquals(true, dateValidator.isValid(employee));
+    }
+
+    @Test
+    @DisplayName("Join date after future")
+    void testJoiningDateAfterFuture(){
+        // Joining Date: 11/9/2048
+        Employee employee = employeeParser.parse("4,Ms.,Dana,Z,White,F,dwh1te34@msn.com,2/18/1998,11/9/2048,378162");
+        Assertions.assertEquals(false, dateValidator.isValid(employee));
     }
 
 }
