5163984c15d00a9bd720563de510c15bfc0cf639
Baaartosz <bartosz_p@hotmail.com>
2022-07-13 08:11:28 +0100

    date validator done.

--- src/main/java/over/achievers/database/validation/DateValidator.java
+++ src/main/java/over/achievers/database/validation/DateValidator.java
@@ -1,10 +1,13 @@
 package over.achievers.database.validation;
 
 import over.achievers.database.model.Employee;
+//  JoiningDate: cannot be before the dateOfBirth and cannot be in the future;
 
 public class DateValidator implements Validator{
     @Override
     public boolean isValid(Employee employee) {
-        return true;
+        boolean joiningDateAfterDoB = employee.getJoiningDate().after(employee.getDateOfBirth());
+        boolean joiningDateBeforeFuture = employee.getJoiningDate().before(java.util.Calendar.getInstance().getTime());
+        return joiningDateAfterDoB && joiningDateBeforeFuture;
     }
 }
