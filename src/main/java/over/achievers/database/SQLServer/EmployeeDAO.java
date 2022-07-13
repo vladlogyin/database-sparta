88263519a43781ac42b13c9fe09936ba70e23990
wiater88 <wiater88@gmail.com>
2022-07-13 14:11:11 +0100

    Prepared statement added

--- src/main/java/over/achievers/database/SQLServer/EmployeeDAO.java
+++ src/main/java/over/achievers/database/SQLServer/EmployeeDAO.java
@@ -3,14 +3,14 @@ package over.achievers.database.SQLServer;
 
 import over.achievers.database.model.Employee;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 public class EmployeeDAO implements DAOInterface {
     private ConnectionFactory connectionFactory;
+
+    private final String INSERT_SQL_STATEMENT = "INSERT INTO employee (emp_number,name_preference,first_name,middle_name,last_name,gender,email,date_of_birth,joining_date,salary) VALUES (?,?,?,?,?,?,?,?,?,?)";
+    private final String GET_EMPLOYEE_BY_ID = "SELECT * FROM employee WHERE emp_number=?";
 
     public void saveFromCollection(List<Employee> employeeList){
         for (Employee emp : employeeList){
@@ -20,19 +20,19 @@ public class EmployeeDAO implements DAOInterface {
     @Override
     public void saveEmployee(Employee employee) {
         Connection conn = ConnectionFactory.getConnection();
-        try { // emp_number,name_preference
-            Statement statement = conn.createStatement();
-            statement.executeUpdate("INSERT INTO employee (emp_number,name_preference,first_name,middle_name,last_name,gender,email,date_of_birth,joining_date,salary) " +
-                    "VALUES (" + employee.getEmpNumber()
-                    + ", " + "'" + employee.getNamePreference()
-                    + "'" + ", " + "'" + employee.getFirstName()
-                    + "'" + ", " + "'" + employee.getMiddleName()
-                    + "'" + ", " + "'" + employee.getLastName()
-                    + "'" + ", " + "'" + employee.getGender()
-                    + "'" + ", " + "'" + employee.getEmail()
-                    + "'" + ", " + "'" + employee.getDateOfBirth()
-                    + "'" + ", " + "'" + employee.getJoiningDate()
-                    + "'" + ", " + employee.getSalary() + ")");
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(INSERT_SQL_STATEMENT);
+            preparedStatement.setInt(1,employee.getEmpNumber());
+            preparedStatement.setString(2, String.valueOf((employee.getNamePreference())));
+            preparedStatement.setString(3,employee.getFirstName());
+            preparedStatement.setString(4, String.valueOf(employee.getMiddleName()));
+            preparedStatement.setString(5,employee.getLastName());
+            preparedStatement.setString(6, String.valueOf(employee.getGender()));
+            preparedStatement.setString(7,employee.getEmail());
+            preparedStatement.setDate(8,employee.getDateOfBirth());
+            preparedStatement.setDate(9,employee.getJoiningDate());
+            preparedStatement.setInt(10,employee.getSalary());
+            preparedStatement.execute();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -40,18 +40,19 @@ public class EmployeeDAO implements DAOInterface {
 
     @Override
     public void getEmployeeByID(Integer id) {
+
         Connection conn = ConnectionFactory.getConnection();
         try {
             Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery(String.format("SELECT * FROM employee WHERE emp_number=%d", id));
+            PreparedStatement preparedStatement = conn.prepareStatement(GET_EMPLOYEE_BY_ID);
+            preparedStatement.setInt(1,id);
+            ResultSet rs = preparedStatement.executeQuery();
             rs.next();
             StringBuffer sb = new StringBuffer();
             for (int i = 1; i < 11; i++) {
-                sb.append(rs.getString(i) + " ");
+                sb.append(rs.getString(i)).append(" ");
 
             }
-//            String result = rs.getString(1) + rs.getString(2) + rs.getString(3) + rs.getString(4) + rs.getString(5)
-            System.out.println(sb.toString());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
