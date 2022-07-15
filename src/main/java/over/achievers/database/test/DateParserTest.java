f94995c8cdc4b16dc93be4f2efdbfbf82bee46e2
OTDZ <omartehami6@gmail.com>
2022-07-15 12:28:49 +0100

    Tested DateParser correctly unparses a Date

--- src/main/java/over/achievers/database/test/DateParserTest.java
+++ src/main/java/over/achievers/database/test/DateParserTest.java
@@ -17,28 +17,34 @@ class DateParserTest {
     @Test
     @DisplayName("Parse valid date")
     void testParseValidDate(){
-        Assertions.assertEquals(Date.valueOf(LocalDate.of(2021,1,1)), DateParser.parse("1/1/2021"));
+        Assertions.assertEquals(Date.valueOf(LocalDate.of(2021,1,1)), DateParser.parse("01/01/2021"));
     }
 
     @Test
     @DisplayName("Parse invalid day")
     void testParseInvalidDay(){
-        Exception e = Assertions.assertThrows(InvalidFormatException.class, () -> DateParser.parse("1/55/2021"));
+        Exception e = Assertions.assertThrows(InvalidFormatException.class, () -> DateParser.parse("01/55/2021"));
         Assertions.assertEquals("Invalid format exception: Issue with date format: Invalid value for DayOfMonth (valid values 1 - 28/31): 55", e.getMessage());
     }
 
     @Test
     @DisplayName("Parse invalid month")
     void testParseInvalidMonth(){
-        Exception e = Assertions.assertThrows(InvalidFormatException.class, () -> DateParser.parse("15/1/2021"));
+        Exception e = Assertions.assertThrows(InvalidFormatException.class, () -> DateParser.parse("15/01/2021"));
         Assertions.assertEquals("Invalid format exception: Issue with date format: Invalid value for MonthOfYear (valid values 1 - 12): 15", e.getMessage());
     }
 
     @Test
     @DisplayName("Parse invalid year")
     void testParseInvalidYear(){
-        Exception e = Assertions.assertThrows(InvalidFormatException.class, () -> DateParser.parse("1/1/2021.5"));
+        Exception e = Assertions.assertThrows(InvalidFormatException.class, () -> DateParser.parse("01/01/2021.5"));
         Assertions.assertEquals("Invalid format exception: Issue with date format: For input string: \"2021.5\"", e.getMessage());
+    }
+
+    @Test
+    @DisplayName("Unparse date")
+    void testUnparseDate(){
+        Assertions.assertEquals("01/01/2021", DateParser.unparse(Date.valueOf(LocalDate.of(2021,1,1))));
     }
 
 }
