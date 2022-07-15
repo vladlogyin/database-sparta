c7a3c338a50a1ddef6d41a8e903081c5807ac4f5
OTDZ <omartehami6@gmail.com>
2022-07-15 11:48:43 +0100

    Tested Parser throws NullPointerException when parse input is null

--- src/main/java/over/achievers/database/test/ParserTest.java
+++ src/main/java/over/achievers/database/test/ParserTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import over.achievers.database.model.Employee;
 import over.achievers.database.parsing.DateParser;
+import over.achievers.database.parsing.InvalidFormatException;
 import over.achievers.database.parsing.Parser;
 
 import java.util.Date;
@@ -23,9 +24,16 @@ class ParserTest {
 
     @Test
     @DisplayName("Valid employee")
-    void testValidEmployee(){
+    void testParseValidEmployee(){
         Employee employee = new Employee(1, "Mrs.", "Ronda", 'W', "Jackson", 'F', "rjackson77@hotmail.com", DateParser.parse("10/10/1982"), DateParser.parse("4/1/2009"), 100123);
         Assertions.assertEquals(employee.toString(), employeeParser.parse("1,Mrs.,Ronda,W,Jackson,F,rjackson77@hotmail.com,10/10/1982,4/1/2009,100123").toString());
+    }
+
+    @Test
+    @DisplayName("Null")
+    void testParseNull(){
+        // Throws a NullPointerException
+        Assertions.assertThrows(NullPointerException.class, () -> employeeParser.parse(null));
     }
 
 }
